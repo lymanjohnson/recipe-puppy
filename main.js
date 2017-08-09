4e5b6a1da46d6fa50304b5a0f024e38635e02e26
lymanjohnson <lyman.johnson@gmail.com>
2017-08-09 09:50:52 -0400

    starting over...

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let searchBar = document.getElementById("search-bar");
 
 searchBar.addEventListener('keypress', function(e) {
   let key = e.which || e.keyCode;
-  if (key === 13) {fetchSearch()}} );
+  if (key === 13) {fetchSearch(createQuery(searchBar.value))}} );
 
 function createQuery(searchTerms){
   let query = "http://www.recipepuppy.com/api/?q=";
@@ -11,57 +11,6 @@ function createQuery(searchTerms){
   return query;
 }
 
-function fetchSearch(){
-fetch(createQuery(searchBar))
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
-
-      // Examine the text in the response
-      response.json().then(function(data) {
-        console.log(data.results[0].title);
-        let title = data.results[0].title;
-        console.log(title);
-        console.log(`The first Star Wars movie is ${title}`);
-
-        //render characters
-
-        let characters = data.results[0].characters;
-
-
-
-        function renderCharacters(){
-          return `
-            <ul>
-              ${characters.map(character => `<li>${character}</li>`).join('')}
-            </ul>
-          `;
-        }
-
-        console.log(characters);
-
-
-      //let's set up the HTML
-      //characters as array then as li
-
-         let markup = `
-           <div class="title">
-             <p>Title: ${title}</p>
-             <p>Episode ID: ${data.results[0].episode_id}</p>
-             <ul>Characters:
-               <li>${data.results[0].characters}</li>
-             </ul>
-             ${renderCharacters(data.results.characters)}
-           </div>
-         `
-
-         document.body.innerHTML = markup;
-
-      });
-    }
-  )
+function fetchSearch(url) {
+  console.log(url);
 }
