4ef131cf69e11e7268435eafb08b8d7299826af3
lymanjohnson <lyman.johnson@gmail.com>
2017-08-08 20:18:02 -0400

    It generates valid queries

--- main.js
+++ main.js
@@ -5,5 +5,14 @@ searchBar.addEventListener('keypress', function(e) {
   if (key === 13) {fetchSearch()}} );
 
 function fetchSearch() {
-  console.log(searchBar.value);
+  console.log(createQuery(searchBar.value));
 }
+
+function createQuery(searchTerms){
+  let query = "http://www.recipepuppy.com/api/?q=";
+  searchTerms = searchTerms.replace(/\s+/g,"+");
+  query += searchTerms;
+  return query;
+}
+
+//http://www.recipepuppy.com/api/?q=omelet
